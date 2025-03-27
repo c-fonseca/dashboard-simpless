ba0157a6b3c9252c399a637f50c3fd1a3639e381
Cleissiane <cleissianeefonseca17@gmail.com>
2025-03-27 00:55:07 -0300

    add funcionalidade js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.getElementById("botao-entrar").addEventListener("click", function(){
     window.location.href = "dashboard.html";
 });
+
+document.getElementById("botao-entrar").addEventListener("click", function(){
+    window.location.href = "dashboard.html";
+});
+
+function gerarRelatorio(){
+    alert ("Relatório gerado com sucesso!");
+}
+
+function configuracoesSalvas() {
+    if(confirm("Alterações feitas com sucesso. Deseja cancelar?")){
+        alert ("Alterações Confirmadas.");
+    }else{
+        alert("Alterações Desfeitas.");
+    }
+}
+
